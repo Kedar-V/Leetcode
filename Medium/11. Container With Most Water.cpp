739631d30c0aff9a70f09ee7dc64e35757f6d664
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2022-03-01 13:36:21 +0530

    Update 11. Container With Most Water.cpp

--- Medium/11. Container With Most Water.cpp
+++ Medium/11. Container With Most Water.cpp
@@ -16,11 +16,6 @@ public:
         
         while (start < end){
             int curr_area = getArea(height, start, end);
-            // int A1 = getArea(height, start + 1, end);
-            // int A2 = getArea(height, start, end - 1);
-            //cout << curr_area << " " << A1 << " " << A2 << endl;
-            //cout << start << " " << end << "--->" << curr_area << endl;
-            //int max_area = max({A1, A2, curr_area});
             Max = max(Max, curr_area);
             if ( height[start] < height[end] ){
                 start += 1;
